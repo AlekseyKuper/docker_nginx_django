da4afb8875f39ded256136d71ac6ed4362448287
Aleksey <aleksk.ru95@mail.ru>
2024-01-13 21:35:39 +0300

    Change django part

--- code/mysite/urls.py
+++ code/mysite/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from post.views import AnyDataView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', AnyDataView.as_view(), name='any_data'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
